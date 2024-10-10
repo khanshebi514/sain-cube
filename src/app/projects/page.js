44e6c96f1051b97c66540571b7119b4f9a79337b
khanshebi514 <khanshebi514@gmail.com>
2024-10-10 15:57:16 +0500

    project page updated

--- src/app/projects/page.js
+++ src/app/projects/page.js
@@ -1,5 +1,23 @@
+import BreedCrumb from "../components/BreedCrumb";
+import { PROJECTS } from "../constants";
+import ProjectCard from "../ui/ProjectCard";
+
 export default function Projects() {
   return (
-    <div>page</div>
+    <section className='bg-[#1E1c22]'>
+    <div className='mt-[5%]'>
+      <div>
+        <BreedCrumb heading={'Industry Specific Projects'} link={'Projects'} description={''} />
+      </div>
+      <div className="mt-10">
+      <div className='container flex justify-center gap-5 flex-wrap'>
+        {PROJECTS.map((service, index)=>
+        <ProjectCard key={index} index={index} title={service.title} description={service.title} ImgUrl={service.iconUrl} video={service.video} industry={service.industy}/>
+        )}
+      </div>
+      </div>
+      
+    </div>
+  </section>
   )
 }
